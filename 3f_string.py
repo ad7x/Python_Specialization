47eb46dd35a2382befb619c13365542e6cba5fee
ad7x <aadityaraj337@gmail.com>
2026-03-10 22:41:35 +0530

    fstring

--- 3f_string.py
+++ 3f_string.py
@@ -1,2 +1,7 @@
 s=10
 print(f"this is a fstring with value embedded of {s}")
+
+# '#' is use for inline comment in python
+
+variable = "put in string"
+print(f"f strings allow you to embed variables {variable}")
